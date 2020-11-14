79f43b1a03a1340ceb83e101d1a89bf8ce3cb3fa
Chinmay Patil <chinmaytlc@gmail.com>
2020-11-13 23:18:00 -0700

    Added more stuff to the multitask

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import subprocess
+import atexit
+import os
+import signal
 
-subprocess.Popen(["python3", "ArtaeumRaidTool.py", "stdout"])
-subprocess.Popen(["python3", "SimpleVote.py", "stdout"])
+def kill_child():
+    if child1_pid is None:
+        pass
+    else:
+        os.kill(child1_pid, signal.SIGTERM)
+    if child2_pid is None:
+        pass
+    else:
+        os.kill(child2_pid, signal.SIGTERM)
+
+child1 = subprocess.Popen(["python3", "ArtaeumRaidTool.py", "stdout"])
+child2 = subprocess.Popen(["python3", "SimpleVote.py", "stdout"])
+
+
+global child1_pid
+global child2_pid
+child1_pid = child1.pid
+child2_pid = child2.pid
+
+atexit.register(kill_child)
+
+child1.wait(); child2.wait()
