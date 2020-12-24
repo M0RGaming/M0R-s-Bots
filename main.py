09fb1b2972b1dd48424b8ddc8e81a33c0f329d26
Chinmay Patil <chinmaytlc@gmail.com>
2020-12-24 01:53:50 -0700

    Forgot to add the new bot to main.py

--- main.py
+++ main.py
@@ -12,16 +12,22 @@ def kill_child():
         pass
     else:
         os.kill(child2_pid, signal.SIGTERM)
+    if child3_pid is None:
+        pass
+    else:
+        os.kill(child3_pid, signal.SIGTERM)
 
 child1 = subprocess.Popen(["python3", "ArtaeumRaidTool.py", "stdout"])
 child2 = subprocess.Popen(["python3", "SimpleVote.py", "stdout"])
-
+child3 = subprocess.Popen(["python3", "giveawayTool.py", "stdout"])
 
 global child1_pid
 global child2_pid
+global child3_pid
 child1_pid = child1.pid
 child2_pid = child2.pid
+child3_pid = child3.pid
 
 atexit.register(kill_child)
 
-child1.wait(); child2.wait()
+child1.wait(); child2.wait(); child3.wait()
